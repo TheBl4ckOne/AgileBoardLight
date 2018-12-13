2b2c203704fd52b58b2cb479c8730bf1a3c93961
Marc Scheer <marcscheer2003@yahoo.de>
2018-12-13 13:41:46 +0100

    Initial Commit containing the Structure of the Poject

---
+++ src/Programm/Programm.java
@@ -0,0 +1,17 @@
+package Programm;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Programm  extends Application {
+//Hauptklasse in der die main ist und von der aus alles andere gestartet wird
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
