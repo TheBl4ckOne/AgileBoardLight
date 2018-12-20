133800c23118927ddcce1f1ec1cde126eb41bb5a
Marc Scheer <marcscheer2003@yahoo.de>
2018-12-20 07:54:56 +0100

    Packages umbenannt

---
+++ src/views/ProjektScreen.java
@@ -0,0 +1,13 @@
+package views;
+
+
+import javafx.stage.Stage;
+
+public class ProjektScreen {
+
+    private Stage _mainStage;
+
+    public ProjektScreen (Stage stage){
+        _mainStage = stage;
+    }
+}
